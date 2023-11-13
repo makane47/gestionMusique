c9754e55df047c363c95274e0996d9ea35dd0808
Kane Mamadou <dingotrois@gmail.com>
2023-11-13 11:50:15 -0500

    Gestion des albums et artistes

---
+++ src/Controller/AlbumController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AlbumRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AlbumController extends AbstractController
+{
+    /**
+     * @Route("/albums", name="albums", methods={"GET"})
+     */
+    public function listeAlbums(AlbumRepository $repo): Response
+    {
+        $albums=$repo->findAll();
+        return $this->render('album/listeAlbums.html.twig', [
+            // 'controller_name' => 'AlbumController',
+            'lesAlbums' => $albums
+        ]);
+    }
+
+
+    /**
+     * @Route("/album/{id}", name="ficheAlbum", methods={"GET"})
+     */
+
+     public function ficheAlbum($id, AlbumRepository $repo): Response
+     {
+        $album=$repo->find($id);
+         return $this->render('album/ficheAlbum.html.twig', [
+             'leAlbum' => $album
+         ]);
+     }
+ 
+}
---
+++ src/Controller/ArtisteController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Artiste;
+use App\Repository\ArtisteRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ArtisteController extends AbstractController
+{
+    /**
+     * @Route("/artistes", name="artistes", methods={"GET"})
+     */
+    public function listeArtistes(ArtisteRepository $repo): Response
+    {
+        $artistes=$repo->findAll();
+        return $this->render('artiste/listeArtistes.html.twig', [
+            // 'controller_name' => 'ArtisteController',
+            'lesArtistes' => $artistes
+        ]);
+    }
+
+
+    /**
+     * @Route("/artiste/{id}", name="ficheArtiste", methods={"GET"})
+     */
+
+     public function ficheArtiste($id, ArtisteRepository $repo): Response
+     {
+        $artiste=$repo->find($id);
+         return $this->render('artiste/ficheArtiste.html.twig', [
+             'leArtiste' => $artiste
+         ]);
+     }
+ 
+
+            /*
+            public function ficheArtiste(Artiste $artiste): Response
+            {
+        
+                return $this->render('artiste/ficheArtiste.html.twig', [
+                    'leArtiste' => $artiste
+                ]);
+            }
+            */
+
+}
